47e712ced4c38f757b8b212c63bb48785d0cbc7e
Julia Kleber <julia.kleber@gmx.net>
2024-02-21 23:02:57 +0100

    implemented zustand middleware persist

--- src/stores/recipeStore.js
+++ src/stores/recipeStore.js
@@ -1,26 +1,32 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useRecipeStore = create((set) => ({
-  recipes: [],
-  currentRecipe: null,
-  message: "",
-  searchTerm: "",
-  lastLocation: "/",
-  setRecipes: (recipes) => {
-    set((state) => ({ recipes: recipes }));
-  },
-  setCurrentRecipe: (recipe) => {
-    set((state) => ({ currentRecipe: recipe }));
-  },
-  resetMessage: () => {
-    set({ message: "" });
-  },
-  setSearchTerm: (term) => {
-    set((state) => ({ searchTerm: term }));
-  },
-  setLastLocation: (location) => {
-    set((state) => ({ lastLocation: location }));
-  },
-}));
+const useRecipeStore = create(
+  persist(
+    (set) => ({
+      recipes: [],
+      currentRecipe: null,
+      message: "",
+      searchTerm: "",
+      lastLocation: "/",
+      setRecipes: (recipes) => {
+        set((state) => ({ recipes: recipes }));
+      },
+      setCurrentRecipe: (recipe) => {
+        set((state) => ({ currentRecipe: recipe }));
+      },
+      resetMessage: () => {
+        set({ message: "" });
+      },
+      setSearchTerm: (term) => {
+        set((state) => ({ searchTerm: term }));
+      },
+      setLastLocation: (location) => {
+        set((state) => ({ lastLocation: location }));
+      },
+    }),
+    { name: "recipe-store" }
+  )
+);
 
 export default useRecipeStore;
--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -1,12 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  isLoggedIn: false,
-  loginMessage: "",
-  registerMessagePartOne: "",
-  registerMessagePartTwo: "",
-  currentUserId: null,
-  resetUser: () => set({ isLoggedIn: false, currentUserId: null }),
-}));
+const useUserStore = create(
+  persist(
+    (set) => ({
+      isLoggedIn: false,
+      loginMessage: "",
+      registerMessagePartOne: "",
+      registerMessagePartTwo: "",
+      currentUserId: null,
+      resetUser: () => set({ isLoggedIn: false, currentUserId: null }),
+    }),
+    { name: "user-store" }
+  )
+);
 
 export default useUserStore;
